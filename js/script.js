1d090151cab9d52fb2528024f1ab43b4a4791618
Yevgeniy-Davydov <131383721+Yevgeniy-Davydov@users.noreply.github.com>
2024-04-02 13:56:45 +0200

    some js code added

--- js/script.js
+++ js/script.js
@@ -1,2 +1,28 @@
 "use strict"
 
+// DOM variables
+
+
+let startDate = document.querySelector(".start_date");
+let endDate = document.querySelector(".end_date");
+let resultOfCalculation = document.querySelector(".result")
+
+
+
+// functions
+
+const handleStartDate = (event) =>{
+ console.log(event.target.value)
+
+ 
+
+}
+const handleEndDate = (event) =>{
+    console.log(event.target.value)
+  
+  }
+// Event Listeners
+startDate.addEventListener("change", handleStartDate);
+endDate.addEventListener("change", handleEndDate);
+
+
