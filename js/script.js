31b3dcff97b4cfc3fcac8d99852fe4f4c0d3d788
Yevgeniy-Davydov <131383721+Yevgeniy-Davydov@users.noreply.github.com>
2024-04-07 20:29:52 +0200

    validation of end-date and plus-buttons added

--- js/script.js
+++ js/script.js
@@ -4,6 +4,8 @@
 
 let form = document.querySelector(".form")
 let startDate = document.querySelector(".start_date");
+let plusWeek = document.querySelector(".plus_week");
+let plusMonth = document.querySelector(".plus_month");
 let endDate = document.querySelector(".end_date");
 let selectWeekend = document.querySelector(".select_days");
 let selectUnit = document.querySelector(".select_unit");
@@ -12,31 +14,59 @@ let resultOfCalculation = document.querySelector(".result");
 let resultDateCalculation;
 
 
-// // functions still in process
-const calculateWeekend = () => {
-console.log(selectWeekend.value)
-}
+
+
 
 // functions that are working :)
 const handleSubmit = (event) => {
   event.preventDefault();
   resultDateCalculation = durationBetweenDates();
   resultOfCalculation.textContent = ` result: ${resultDateCalculation}`;
-  calculateWeekend();// here is the call for the weekend selector function
+  // calculateWeekend();// here is the call for the weekend selector function
+  // console.log(resultDateCalculation)
+
+};
+const addAWeek = () => {
+
+  let start = new Date(startDate.value).getTime();
+  let end = new Date(start + 604800000);
+
+
+  let endYear = end.getFullYear();
+  let endMonth = end.getMonth() + 1 < 10 ? `0${end.getMonth() + 1}` : `${end.getMonth() + 1}`;
+  let endDay = end.getDate() < 10 ? `0${end.getDate()}` : `${end.getDate()}`;
+
+
+  endDate.value = `${endYear}-${endMonth}-${endDay}`;
+
+};
+const addAnMonth = () => {
+  let start = new Date(startDate.value).getTime();
+  let end = new Date(start + 2629743000);
+
+  let endYear = end.getFullYear();
+  let endMonth = end.getMonth() + 1 < 10 ? `0${end.getMonth() + 1}` : `${end.getMonth() + 1}`;
+  let endDay = end.getDate() < 10 ? `0${end.getDate()}` : `${end.getDate()}`;
+
+  endDate.value = `${endYear}-${endMonth}-${endDay}`;
+}
+
+const validationOfEndDate = () => {
+
+  let temporaryDifference = new Date (endDate.value) - new Date (startDate.value);
+  if (startDate.value === "") {
+    alert("please enter the start date first");
+    form.reset();
+    startDate.focus();
+  }else if(temporaryDifference < 0){
+    alert("start date should be smaller than end date");
+    form.reset();
+    startDate.focus();
+  }
 
 };
 
-
-// // Event Listeners
-form.addEventListener("submit", handleSubmit);
-
-
-// function that generally works, but not now..
-
-
-
-
-  function durationBetweenDates(){
+function durationBetweenDates(){
 
   let start = startDate.value;
   let end = endDate.value;
@@ -46,11 +76,11 @@ form.addEventListener("submit", handleSubmit);
 
   switch (unit){
 
-      case "second" : return `${Math.floor(difference / 1000)} ${unit}`
+      case "seconds" : return `${Math.floor(difference / 1000)} ${unit}`;
 
-      case "minute" : return `${Math.floor(difference / 60000)} ${unit}`;
+      case "minutes" : return `${Math.floor(difference / 60000)} ${unit}`;
 
-      case "hour" : return `${Math.floor(difference / 3600000)} ${unit}`;
+      case "hours" : return `${Math.floor(difference / 3600000)} ${unit}`;
 
       case "day" :  
           let amountDays = Math.floor(difference / 86400000);
@@ -63,6 +93,32 @@ form.addEventListener("submit", handleSubmit);
 
   }
 };
+
+
+
+
+
+// // Event Listeners
+form.addEventListener("submit", handleSubmit);
+endDate.addEventListener("change", validationOfEndDate);
+plusWeek.addEventListener("click", addAWeek);
+plusMonth.addEventListener("click", addAnMonth);
+
+
+
+
+
+// // functions still in process
+// const calculateWeekend = (resultDateCalculation) => {
+
+// switch (selectWeekend.value){
+//   case "all-days" : return resultDateCalculation;
+
+//   case "working-days": return 
+//   }
+// };
+
+
 
 
 
